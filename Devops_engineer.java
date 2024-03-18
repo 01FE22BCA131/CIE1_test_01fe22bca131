0106042ef9516b17230d99bbf138f6d4581ef5cf
sneha <01fe22bca131@kletech.ac.in>
2024-03-18 11:24:17 +0530

    Extended code of empolyee class

---
+++ Devops_engineer.java
@@ -0,0 +1,10 @@
+public class Devops_engineer{
+    float Bonus=5000;
+    int salary =30000;
+    public void add_Bonus(){
+        System.out.println("Bonus = " +Bonus);
+        
+       salary +=Bonus;
+        System.out.println("Salary = " +salary);
+    }
+}
